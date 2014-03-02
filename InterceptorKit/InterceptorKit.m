46d97eeed3ce8e43bb13098a1469bf5d914cff87
iska <iska.gp@gmail.com>
2014-03-02 14:24:22 +0100

    Create Xcode project for InterceprotKit

---
+++ InterceptorKit/InterceptorKit.m
@@ -0,0 +1,13 @@
+//
+//  InterceptorKit.m
+//  InterceptorKit
+//
+//  Created by Iska on 02/03/14.
+//  Copyright (c) 2014 BrainCookie. All rights reserved.
+//
+
+#import "InterceptorKit.h"
+
+@implementation InterceptorKit
+
+@end
